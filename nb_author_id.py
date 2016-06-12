047e163724cc8995adc1adf7b694b217d87fd7ce
rishabh230795 <rishabh230795@gmail.com>
2016-06-12 23:46:19 +0530

    Update nb_author_id.py

--- nb_author_id.py
+++ nb_author_id.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
-
-    Use a Naive Bayes Classifier to identify emails by their authors
-    
+"""    
     authors and labels:
     Sara has label 0
     Chris has label 1
